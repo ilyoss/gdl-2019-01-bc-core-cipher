8b08999ecb00bc50c2e6d582c3155eb610365e4d
ilyoss <ily.trevino@live.com>
2019-01-28 20:33:15 -0600

    Updated CSS and start of cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,17 @@
 window.cipher = {
-  // ... 
+
+  cipher.encode(offset, string) = {
+
+    let newString = '';
+    let i;
+
+    for(i = 0; i < string.length; i++){
+      let x = string.charCodeAt(i);
+      let y = (x - 65 + offset)%26 + 65;
+      newString = newString+y;
+    }
+
+    displayResult(newString);
+  }
+
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 
 function reset(){
   document.getElementById("menu").style.display = 'none';
+  document.getElementById("information").style.display = 'none';
+
 }
 
 //La inicio para que el programa corra "limpio"
@@ -36,10 +38,46 @@ function menu(root, menu){
   greeting();
 }
 
-function greeting(){
+//Funcion que se activa al decidir si se va a cifrar o decifrar un mensaje
+function infoInput(menu, info){
+  toggleVisibility(menu, info);
+}
 
+//Funcion para mostrar mensaje de bienvenida, usando el nombre ingresado en el inicio
+function greeting(){
   const nameInput = document.getElementById('userName').value; //Guardo el nombre en la variable nameInput
   console.log(nameInput);
   let greeting = "Hola, " + nameInput + "! ¿Qué te gustaría hacer con tu mensaje?";
   document.getElementById("bienvenida").innerHTML = greeting;
+}
+
+//Funcion que determina el mensaje que se muestra para cifrar
+function cipher(){
+  let title = "Ingresa tu mensaje a cifrar. Recuerda usar mayúsculas únicamente y no utilizar 'ñ' o caracteres especiales.";
+  document.getElementById("inputInfo").innerHTML = title;
+  document.getElementById("enterInfo").innerHTML = "Cifrar";
+}
+
+//Funcion para mostrar como texto el valor del slider al usuario
+function sliderValue(){
+
+  let slider = document.getElementById("offset");
+  let output = document.getElementById("sliderV");
+  output.innerHTML = slider.value;
+
+  slider.oninput = function() {
+  output.innerHTML = this.value;
+  }
+}
+
+function getValues(){
+  let offset = document.getElementById("sliderV").innerHTML.value;
+  let string = document.getElementById("userText").innerHTML;
+
+  cipher.encode(offset,string);
+}
+
+function displayResult(string){
+  document.getElementById("result").innerHTML.value = string;
+  console.log(string);
 }
